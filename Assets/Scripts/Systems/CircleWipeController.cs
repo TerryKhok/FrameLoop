213eb37f7e0bd79381fcff6d5cfc851004bd4a0e
Arashi <fujishita.arashi.30056@gmail.com>
2024-06-08 12:56:34 +0900

    トランジション完成

---
+++ Assets/Scripts/Systems/CircleWipeController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CircleWipeController : MonoBehaviour
+{
+    [SerializeField]
+    private Material _circleWipeMaterial;
+    [SerializeField]
+    private float _duration = 1.0f;
+    [SerializeField]
+    private Image _circleWipeImage;
+    private float _time = 0f;
+    private bool _isTransitioning = false;
+
+    private void Start()
+    {
+        Vector2 playerPos =  PlayerInfo.Instance.g_transform.position;
+        Vector2 playerViewPortPos = Camera.main.WorldToViewportPoint(playerPos);
+
+        _circleWipeMaterial.SetVector("_center", playerViewPortPos);
+
+        BeginTransition();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            BeginTransition();
+        }
+
+        if (_isTransitioning)
+        {
+            _time += Time.deltaTime;
+            float cutoff = Mathf.Clamp01(_time / _duration);
+            _circleWipeMaterial.SetFloat("_cutoff", cutoff);
+
+            if(_time > _duration)
+            {
+                _circleWipeImage.enabled = false;
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _circleWipeMaterial.SetFloat("_cutoff", 0);
+    }
+
+    public void BeginTransition()
+    {
+        _time = 0f;
+        _isTransitioning = true;
+    }
+}
