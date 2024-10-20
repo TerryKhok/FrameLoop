63d6bbd97fb8b49d8dda6c8e661f66b174786cce
Arashi <fujishita.arashi.30056@gmail.com>
2024-10-20 16:33:29 +0900

    壊れる足場のデザイン追加

--- Assets/Scripts/Stage/BreakablePlatform.cs
+++ Assets/Scripts/Stage/BreakablePlatform.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class BreakablePlatform : MonoBehaviour
 {
+    [SerializeField,Tooltip("â°ïù")]
+    private int _width = 1;
+
+    private SpriteRenderer _spriteRenderer;
+    private BoxCollider2D _boxCollider;
+
+    private void OnValidate()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _boxCollider = GetComponent<BoxCollider2D>();
+
+        _spriteRenderer.size = new Vector2(_width,1);
+        _boxCollider.size = new Vector2(_width, 1);
+
+        transform.localScale = Vector3.one;
+    }
+
     public void Break()
     {
         Destroy(gameObject);
