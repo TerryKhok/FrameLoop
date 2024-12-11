fea666f9ce8e89e813aced61c17552d542726019
アラン <146911226+kura193@users.noreply.github.com>
2024-12-12 02:06:55 +0900

    s

---
+++ Assets/Scenes/Cutscenes/Scripts/IntroCutscene.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using Cinemachine;
+
+public class IntroCutscene : MonoBehaviour
+{
+    [SerializeField]
+    private float appearanceTime;
+
+    [System.Serializable]
+    public struct MoveInfo
+    {
+        public float time;
+        public int direction;
+    }
+
+    [SerializeField]
+    private MoveInfo[] _moveInfos;
+    [SerializeField]
+    private float[] _frameTiming;
+    [SerializeField]
+    private float[] _jumpTiming;
+    [SerializeField]
+    private float _sceneChangeTiming;
+    [SerializeField]
+    private GameObject _frameParent;
+    [SerializeField]
+    private CinemachineVirtualCamera _virtualCamera;
+
+    private FrameLoop _frameLoop = null;
+    private PlayerMove _playerMove = null;
+    private PlayerJump _playerJump = null;
+    private float _elapsedTime = 0;
+    private int _moveIndex = 0, _frameIndex = 0, _jumpIndex = 0;
+
+    private GameObject playerSpriteObject;
+
+    private void Start()
+    {
+        _frameLoop = FrameLoop.Instance;
+        _playerJump = GetComponent<PlayerJump>();
+        _playerMove = GetComponent<PlayerMove>();
+        _frameParent.SetActive(false);
+
+        playerSpriteObject = GameObject.Find("PlayerSprite");
+        playerSpriteObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        if(_elapsedTime >= appearanceTime)
+        {
+            playerSpriteObject.SetActive(true);
+        }
+
+        // åoâﬂéûä‘Ç∆î‰ärÇµÇƒÅAâﬂÇ¨ÇƒÇ¢ÇÍÇŒóvëfî‘çÜÇëùÇ‚Ç∑
+        if (_moveInfos[_moveIndex].time <= _elapsedTime)
+        {
+            ++_moveIndex;
+        }
+        _playerMove.SetMove(_moveInfos[_moveIndex].direction);
+
+        if (_jumpTiming[_jumpIndex] <= _elapsedTime)
+        {
+            _playerJump.SetJump();
+            ++_jumpIndex;
+        }
+
+        if (_frameTiming[_frameIndex] <= _elapsedTime)
+        {
+            if (_frameIndex % 2 == 0)
+            {
+                _virtualCamera.enabled = false;
+            }
+            else
+            {
+                _virtualCamera.enabled = true;
+            }
+
+            _frameLoop.SwitchFrame();
+            ++_frameIndex;
+        }
+
+        if (_sceneChangeTiming <= _elapsedTime)
+        {
+            SceneLoader.Instance.LoadScene("lvl 1");
+        }
+    }
+
+    public void GetFrameMachine()
+    {
+        _frameParent.SetActive(true);
+    }
+}
